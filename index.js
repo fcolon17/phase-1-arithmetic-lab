667bd0ecb85a0146d8a5212f05e611e7b1bf63d4
Franklyn Colon <franklyncolon6@gmail.com>
2021-09-26 14:47:29 -0400

    commit tree a33b6b6a061c84b7b5534cd5819497c63786f813

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-
-// Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+Math.floor(Math.random() * 10) + 1;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 10;
+let num4 = 6;
+let mod = num3 % num4;
+Math.max(1, 20, 5, 6);
+let max = Math.max(1, 20, 5, 6);
